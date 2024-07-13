af4a84fc1a5ca0cc31907d4933059fc47be6c605
clagumb <clagumb@gmail.com>
2024-07-13 09:48:17 +0200

    Umbau auf REST

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     private static DataAccessObject dao;
     public static void main(String[] args) {
-        dao = new DataAccessObject(null);
-        dao.findAll();
+        dao = new DataAccessObject("http://localhost:8080/studenten");
+        System.out.println(dao.findAll());
     }
 }
