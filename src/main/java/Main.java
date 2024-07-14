b0550700bcd71cd0bfd62709ebaba74c5b9ea43a
ClaGumb <c.gumbmann@sbs-herzogenaurach.de>
2024-07-14 16:11:02 +0200

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import de.sbs.fswi1.models.StudentDTO;
 import de.sbs.fswi1.services.DataAccessObject;
 
 public class Main {
 
     private static DataAccessObject dao;
+
     public static void main(String[] args) {
         dao = new DataAccessObject("http://localhost:8080/studenten");
+        StudentDTO student = new StudentDTO("Reiner", "Unsinn", "12.12.2002", "FSWI-2");
+        dao.save(student);
+
         System.out.println(dao.findAll());
     }
 }
